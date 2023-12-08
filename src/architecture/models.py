5772cb0a0a67691f638b3eee6584aaf842e26c2e
antoine311200 <antoine311200@gmail.com>
2023-12-08 18:25:41 +0100

    Audio regression for age prediction on CoughVid dataset

--- src/architecture/models.py
+++ src/architecture/models.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 
 from src.architecture.modules import MixtureDensityHead
 
@@ -104,6 +105,183 @@ class LinearNet(torch.nn.Module):
 
         return output
 
+
+class FlattenLinear(LinearNet):
+    def __init__(
+        self, input_dimension: tuple[int], hidden_dims: list[int], output_dimension: int
+    ):
+        self.input_dimension = input_dimension
+        flattened_dimension = 1
+        for dim in input_dimension:
+            flattened_dimension *= dim
+
+        super().__init__(flattened_dimension, hidden_dims, output_dimension)
+
+    def forward(self, x):
+        return super().forward(x.flatten(start_dim=1))
+
+
+class FlattenLinearMDN(LinearMDN):
+    def __init__(
+        self,
+        input_dimension: tuple[int],
+        hidden_dims: list[int],
+        output_dimension: int,
+        n_mixtures: int,
+    ):
+        self.input_dimension = input_dimension
+        flattened_dimension = 1
+        for dim in input_dimension:
+            flattened_dimension *= dim
+
+        super().__init__(flattened_dimension, hidden_dims, output_dimension, n_mixtures)
+
+    def forward(self, x):
+        return super().forward(x.flatten(start_dim=1))
+
+
+class BasicCNN(torch.nn.Module):
+    def __init__(self, input_dimension: tuple[int], output_dimension: int):
+        self.sequential = torch.nn.Sequential(
+            torch.nn.Conv2d(input_dimension[0], 16, 3),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(16, 32, 3),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(32, 64, 3),
+            torch.nn.AdaptiveAvgPool2d((1, 1)),
+            torch.nn.Linear(64, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, output_dimension),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.sequential(x)
+
+
+class ConvolutionalMDN(BasicCNN):
+    def __init__(
+        self,
+        input_dimension: tuple[int],
+        output_dimension: int,
+        n_mixtures: int,
+    ):
+        super().__init__(input_dimension, output_dimension)
+        self.mdn_head = MixtureDensityHead(output_dimension, n_mixtures)
+
+    def forward(
+        self, x: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        x = super().forward(x)
+
+        return self.mdn_head(x)
+
+
+class CoughMDN(nn.Module):
+    def __init__(self, n_mixtures: int):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.flatten = nn.Flatten()
+        self.linear1 = nn.Linear(in_features=128 * 5 * 4, out_features=128)
+        self.linear2 = nn.Linear(in_features=128, out_features=3*n_mixtures)#3 * n_mixtures)
+
+        self.bn = nn.BatchNorm1d(3*n_mixtures)
+        self.mdn_head = MixtureDensityHead(1, n_mixtures)
+
+
+    def forward(self, input_data):
+        x = self.conv1(input_data)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.flatten(x)
+        x = self.linear1(x)
+        # x = nn.functional.relu(x)
+        x = self.linear2(x)
+        # Add batch norm
+        x = self.bn(x)
+
+
+        # Return sigmoid
+        # return torch.sigmoid(x)
+        out = self.mdn_head(x)
+
+        return out
+
+class CoughCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.flatten = nn.Flatten()
+        self.linear1 = nn.Linear(in_features=128 * 5 * 4, out_features=128)
+        self.linear2 = nn.Linear(in_features=128, out_features=1)
+
+
+    def forward(self, input_data):
+        x = self.conv1(input_data)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.flatten(x)
+        x = self.linear1(x)
+        x = self.linear2(x)
+
+        return torch.sigmoid(x)
 
 if __name__ == "__main__":
     input_tensor = torch.randn(32, 3)
--- src/datasets.py
+++ src/datasets.py
@@ -1,7 +1,13 @@
 from typing import Callable
 
+import os
+import json
+
 import torch
 from torch.utils.data import Dataset
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+import torchaudio as ta
 
 
 class FunctionDataset(Dataset):
@@ -54,3 +60,100 @@ class FunctionDataset(Dataset):
 
 def sinusoid(x: torch.Tensor) -> torch.Tensor:
     return x + 0.3 * torch.sin(2 * torch.pi * x)
+
+
+class MNISTDataset(Dataset):
+    def __init__(self, train: bool = True):
+        super().__init__()
+        self.mnist = MNIST(root="data", download=True, train=train)
+        self.transform = ToTensor()
+
+    def __len__(self):
+        return len(self.mnist)
+
+    def __getitem__(self, index):
+        image, label = self.mnist[index]
+
+        image = self.transform(image)
+        label = torch.tensor(label)
+
+        return image, label
+
+
+class CoughDataset(Dataset):
+    def __init__(
+        self,
+        audio_path: str,
+        label_path: str,
+        transformation,
+        target_sample_rate,
+        num_samples,
+        device = 'cpu',
+    ):
+        name_set = list(
+            set([file for file in os.listdir(audio_path) if file.endswith("wav")])
+        )
+
+        self.datalist = name_set
+        self.audio_path = audio_path
+        self.label_path = label_path
+        self.device = device
+        self.transformation = transformation.to(device)
+        self.target_sample_rate = target_sample_rate
+        self.num_samples = num_samples
+
+    def __len__(self):
+        return len(self.datalist)
+
+    def __getitem__(self, idx):
+        if idx >= len(self.datalist):
+            raise StopIteration
+
+        audio_file_path = os.path.join(self.audio_path, self.datalist[idx])
+        label_file_path = os.path.join(
+            self.label_path, self.datalist[idx][:-4] + ".json"
+        )
+
+        with open(label_file_path, "r") as f:
+            content = json.loads(f.read())
+            f.close()
+
+        if "age" not in content:
+            return None  # Skip this item
+        else:
+            label = float(content["age"])
+
+        waveform, sample_rate = ta.load(
+            audio_file_path
+        )  # (num_channels,samples) -> (1,samples) makes the waveform mono
+
+        waveform = self._resample(waveform, sample_rate)
+        waveform = self._mix_down(waveform)
+        waveform = self._cut(waveform)
+        waveform = self._right_pad(waveform)
+
+        waveform = waveform.to(self.device)
+        waveform = self.transformation(waveform)
+
+        return waveform, torch.tensor(label).reshape((1,))
+
+    def _resample(self, waveform, sample_rate):
+        resampler = ta.transforms.Resample(sample_rate, self.target_sample_rate)
+        return resampler(waveform)
+
+    def _mix_down(self, waveform):
+        waveform = torch.mean(waveform, dim=0, keepdim=True)
+        return waveform
+
+    def _cut(self, waveform):
+        if waveform.shape[1] > self.num_samples:
+            waveform = waveform[:, : self.num_samples]
+        return waveform
+
+    def _right_pad(self, waveform):
+        signal_length = waveform.shape[1]
+        if signal_length < self.num_samples:
+            num_padding = self.num_samples - signal_length
+            last_dim_padding = (0, num_padding,)
+            waveform = torch.nn.functional.pad(waveform, last_dim_padding)
+        return waveform
